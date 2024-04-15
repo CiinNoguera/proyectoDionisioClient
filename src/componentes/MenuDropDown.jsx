6f7733d134d79d8b361aa6f5036281a233dfdcaf
Cintia Noguera <ciin.noguera@hotmail.com>
2024-04-15 16:38:24 -0300

    menu desplegable

---
+++ src/componentes/MenuDropDown.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+// import "../css/menuDropDown.css";
+
+const MenuDropDown = () => {
+
+  const [menu, setMenu] = useState(false);
+  
+  const menuOpen = () => {
+    setMenu(!menu);
+  };
+  
+  return (
+    <div>
+        <button className="navbar__btn" onClick={menuOpen}>
+            <i className="bi bi-menu-button-wide nabvar__menu"></i>
+        </button>
+       {menu ? (
+        <div className='menu__container'>
+            <Link to="/Login" className = "menu__item">Iniciar secion</Link>;
+            <Link to="/Register" className = "menu__item">Registro</Link>;
+            <Link to="/Cart" className = "menu__item">Carrito</Link>;
+        </div>
+       ): null };
+    </div>
+  );
+};
+
+export default MenuDropDown;
--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../css/navbar.css";
+import MenuDropDown from "./MenuDropDown";
 
 const Navbar = ( {logo} ) => {
     return (
         <>
             <nav className="navbar">
-                   <button className="navbar__btn"><i className="bi bi-menu-button-wide nabvar__menu"></i></button>
+                   <MenuDropDown/>
                    <img src={logo} alt="Logo Dionisio" className="nabvar__logo" />  
             </nav>
         </>
